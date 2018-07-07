f2f0fd338604491d1b5a00c8253067365695d692
Alex Ginsberg <alex.ginsberg@richmond.edu>
2018-07-07 14:53:45 -0400

    getting the info of the current user

--- my-app/src/Dashboard/Dashboard.js
+++ my-app/src/Dashboard/Dashboard.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import './Dashboard.css';
+import firebase from '../firebase.js';
 
 class Dashboard extends Component {
+    constructor(props) {
+        super(props)
+        console.log(props)
+        this.state = {
+            userInfo: {}
+        }
+    }
+
+    componentDidMount() {
+        console.log(this.state)
+        const userRef = firebase.database().ref().child('users')
+        userRef.once('value')
+            .then(snapshot => {
+                const users = snapshot.val()
+                let userInfo = null
+                for (let key in users) {
+                    if (users[key].email === this.props.user.email) {
+                        userInfo = users[key]
+                    }
+                }
+                this.setState({userInfo})
+            })
+    }
+
     render() {
+        let ifUser = this.state.userInfo.email ? <p>hi</p> : <p>bye</p>
         return (
-        <p>dash</p>
+        <div>
+            {ifUser}
+        </div>
+        
         );
     }
 }
